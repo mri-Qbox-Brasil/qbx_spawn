0d43c8c5147f1a2492511c02f09be67f20dca185
SubZer0GLX <subzeropacker@hotmail.com>
2024-12-11 03:29:53 -0300

    feat: Ps Housing

---
+++ server/main.lua
@@ -0,0 +1,32 @@
+local ps_starters = {
+    -- ["Name of Starting appartment"] = vector3()
+    ["Motel"] = vector3(325.14, -229.54, 54.21)
+}
+
+lib.callback.register('qbx_spawn:server:getLastLocation', function(source)
+    local player = exports.qbx_core:GetPlayer(source)
+    return json.decode(MySQL.single.await('SELECT position FROM players WHERE citizenid = ?', {player.PlayerData.citizenid}).position), player.PlayerData.metadata.inside and player.PlayerData.metadata.inside.propertyId
+end)
+
+lib.callback.register('qbx_spawn:server:getHouses', function(source)
+    local player = exports.qbx_core:GetPlayer(source)
+    local houseData = {}
+
+    local houses = MySQL.query.await('SELECT * FROM properties WHERE owner_citizenid = ?', {player.PlayerData.citizenid})
+
+    if not houses then return {} end
+
+    for i = 1, #houses do
+        local house = houses[i]
+            if not house.apartment then
+            local door = exports['ps-housing']:getMainDoor(house.property_id, 1)
+            local coords = door.objCoords or door.coords or door.doors[1] and door.doors[1].coords or door.doors[1].objCoords
+            houseData[#houseData + 1] = {
+                label = house.street,
+                coords = coords
+            }
+        end
+    end
+
+    return houseData
+end)
